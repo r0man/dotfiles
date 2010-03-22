25b791f3cd1cd8411a59d08b63a18d05ce704ab0
Roman Scherer <roman@soundcloud.com>
2010-03-22 20:39:17 +0100

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s .bash_aliases $HOME/.bash_aliases
+ln -s .bash_logout $HOME/.bash_logout
+ln -s .bashrc $HOME/.bashrc
+ln -s .xmodmaprc $HOME/.xmodmaprc
+ln -s .zshrc $HOME/.zshrc
+
