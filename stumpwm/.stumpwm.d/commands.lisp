d487c8644cd12decfb07a4f022ec561b10a67a4d
r0man <roman@burningswell.com>
2019-05-03 12:49:45 +0200

    Add commands.lisp

---
+++ stumpwm/.stumpwm.d/commands.lisp
@@ -0,0 +1,109 @@
+(in-package :stumpwm)
+
+;;; COMMANDS
+
+(defcommand chromium () ()
+  "Run the chromium web browser."
+  (run-shell-command "chromium"))
+
+(defcommand emacs-client () ()
+  "Run the emacs client."
+  (run-shell-command "emacsclient --create-frame"))
+
+(defcommand firefox () ()
+  "Run the firefox web browser."
+  (run-shell-command "firefox"))
+
+(defcommand htop () ()
+  "Run htop."
+  (run-or-raise "urxvt -name HTop -e htop" '(:instance "HTop")))
+
+(defcommand keyboard-backlight (on-or-off)
+    ((:y-or-n " Turn Keyboard Backlight on? "))
+  "Turn the keyboard light on or off."
+  (run-shell-command (format nil "keyboard-backlight ~a" (if on-or-off "on" "off"))))
+
+(defcommand logs () ()
+  "Show the logs."
+  (run-or-raise "urxvt -name Logs -e sudo journalctl -f -a -n 100" '(:instance "Logs")))
+
+(defcommand polybar () ()
+  "Run the polybar status bar."
+  (run-shell-command "start-polybar"))
+
+(defcommand slack () ()
+  "Run slack."
+  (run-or-raise "slack --startup" '(:instance "Slack")))
+
+(defcommand terminal () ()
+  "Run the urxvt terminal."
+  ;; (hsplit)
+  ;; (fnext)
+  (run-shell-command "urxvt"))
+
+(defcommand terminal-raise () ()
+  "Run or raise the urxvt terminal."
+  (run-or-raise "urxvt -name Terminal" '(:instance "Terminal")))
+
+;; (defcommand (fprev tile-group) () ()
+;;   "Cycle through the frame tree to the prev frame."
+;;   (focus-prev-frame (current-group)))
+
+;; (defcommand file-manager () ()
+;;   "Run or raise the file manager."
+;;   (run-or-raise "nautilus" '(:class "Nautilus")))
+
+;; (defcommand volume-lower () ()
+;;   "Lower the volume."
+;;   (message "Lower volume"))
+
+;; (defcommand volume-raise () ()
+;;   "Raise the volume."
+;;   (message "Raise volume"))
+
+;; (defcommand volume-toggle () ()
+;;   "Toggle the volume."
+;;   (message "Toggle volume"))
+
+(defun shift-windows-forward (frames win)
+  (when frames
+    (let ((frame (car frames)))
+      (shift-windows-forward (cdr frames)
+                             (frame-window frame))
+      (when win
+        (pull-window win frame)))))
+
+(defcommand rotate-windows () ()
+  "Rotate windows"
+  (let* ((frames (group-frames (current-group)))
+         (win (frame-window (car (last frames)))))
+    (shift-windows-forward frames win)))
+
+(defcommand swap-windows () ()
+  (let* ((group (current-group))
+         (cur-frame (tile-group-current-frame group))
+         (frames (group-frames group)))
+    (if (eq (length frames) 2)
+        (progn (if (or (neighbour :left cur-frame frames)
+                       (neighbour :right cur-frame frames))
+                   (progn
+                     (only)
+                     (vsplit))
+                   (progn
+                     (only)
+                     (hsplit))))
+        (message "Works only with 2 frames"))))
+
+;; Rofi
+
+(defun rofi (mode)
+  (run-shell-command (concat "rofi -show " mode " -m " (write-to-string (head-number (current-head))))))
+
+(defcommand rofi-run () ()
+  (rofi "run -sidebar-mode"))
+
+(defcommand rofi-window () ()
+  (rofi "window"))
+
+(defcommand rofi-windowcd () ()
+  (rofi "windowcd"))
