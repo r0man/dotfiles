e485c424a8ba67e8897e3a788ebc91cc045fc33a
r0man <roman@burningswell.com>
2021-05-09 10:07:12 +0200

    Add flat guix channel

---
+++ guix/.config/guix/channels.scm
@@ -0,0 +1,9 @@
+(cons* (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (introduction
+         (make-channel-introduction
+          "33f86a4b48205c0dc19d7c036c85393f0766f806"
+          (openpgp-fingerprint
+           "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+       %default-channels)
