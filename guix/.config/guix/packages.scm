aa9c416399529e4f6b4888ad6aaa7f9c2a11467b
r0man <roman@burningswell.com>
2021-05-12 14:57:15 +0200

    Add Emacs packages for guix

--- guix/.config/guix/packages.scm
+++ guix/.config/guix/packages.scm
@@ -1,13 +1,90 @@
 (specifications->manifest
- '("cmake"
-   "cl-stumpwm"
+ '("cl-stumpwm"
+   "cmake"
+   "emacs-aggressive-indent"
+   "emacs-auto-dictionary-mode"
+   "emacs-avy"
+   "emacs-bluetooth"
+   "emacs-cider"
+   "emacs-company-box"
+   "emacs-company-lsp"
+   "emacs-company-quickhelp"
+   "emacs-dap-mode"
+   "emacs-dart-mode"
+   "emacs-docker"
+   "emacs-docker-compose-mode"
+   "emacs-dockerfile-mode"
+   "emacs-edit-indirect"
+   "emacs-elfeed"
+   "emacs-elisp-slime-nav"
+   "emacs-elpy"
+   "emacs-emms"
+   "emacs-engine-mode"
+   "emacs-exec-path-from-shell"
+   "emacs-expand-region"
+   "emacs-find-file-in-project"
+   "emacs-flx"
+   "emacs-flycheck"
+   "emacs-flycheck-flow"
+   "emacs-forge"
+   "emacs-geiser"
+   "emacs-geiser-guile"
+   "emacs-gif-screencast"
+   "emacs-go-mode"
+   "emacs-graphql"
+   "emacs-graphql-mode"
+   "emacs-guix"
+   "emacs-haskell-mode"
+   "emacs-helm"
+   "emacs-helm-lsp"
+   "emacs-helm-projectile"
+   "emacs-htmlize"
+   "emacs-hy-mode"
+   "emacs-ido-ubiquitous"
+   "emacs-ido-vertical-mode"
+   "emacs-kotlin-mode"
+   "emacs-lsp-mode"
+   "emacs-lsp-treemacs"
+   "emacs-lsp-ui"
+   "emacs-macrostep"
+   "emacs-magit"
+   "emacs-markdown-mode"
+   "emacs-markdown-preview-mode"
+   "emacs-multi-term"
+   "emacs-multiple-cursors"
    "emacs-native-comp"
+   "emacs-org"
+   "emacs-org-contrib"
+   "emacs-org-present"
+   "emacs-org-reveal"
+   "emacs-pandoc-mode"
+   "emacs-paredit"
+   "emacs-parsec"
+   "emacs-pass"
+   "emacs-plantuml-mode"
+   "emacs-posframe"
+   "emacs-projectile"
+   "emacs-rainbow-mode"
+   "emacs-redshank"
+   "emacs-sbt-mode"
+   "emacs-scala-mode"
+   "emacs-slime"
+   "emacs-slime-company"
+   "emacs-smex"
    "emacs-stumpwm-mode"
-   "emacs-stumpwm-mode"
+   "emacs-terraform-mode"
+   "emacs-treemacs"
+   "emacs-treemacs"
+   "emacs-undo-tree"
+   "emacs-use-package"
    "emacs-vterm"
+   "emacs-web-mode"
+   "emacs-which-key"
+   "emacs-yaml-mode"
+   "emacs-yasnippet"
+   "emacs-yasnippet-snippets"
    "font-dejavu"
    "font-gnu-freefont"
-   "font-inconsolata"
    "font-inconsolata"
    "font-terminus"
    "fontconfig"
@@ -35,4 +112,37 @@
    "sbcl-stumpwm-wifi"
    "sbcl-stumpwm-winner-mode"
    "stumpish"
-   "stumpwm"))
+   "stumpwm"
+   ;;;;
+   ;;;; Emacs packages not available in guix
+   ;;;;
+   ;; avy-menu
+   ;; cask
+   ;; clj-refactor
+   ;; clojure-mode-extra-font-locking
+   ;; color-theme
+   ;; ein
+   ;; ensime
+   ;; flymd
+   ;; github-browse-file
+   ;; indium
+   ;; kubernetes
+   ;; lsp-docker
+   ;; markdown-preview-eww
+   ;; org-ac
+   ;; org-gcal
+   ;; org-plus-contrib
+   ;; plantuml
+   ;; scss-mode
+   ;; slamhound
+   ;; smooth-scrolling
+   ;; solarized-theme
+   ;; soundklaus
+   ;; timesheet
+   ;; tree-sitter
+   ;; tree-sitter-langs
+   ;; virtualenvwrapper
+   ;; emacs-pretty-lambdada
+   ;; highline
+   ;; popwin
+   ))
